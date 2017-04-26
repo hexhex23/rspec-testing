c7ef549f51689baad1c0f01b5eb12b3015e05a52
hexhex23 <mariawilson23@gmail.com>
2017-04-26 09:16:15 +0800

    complete

--- lib/tree.rb
+++ lib/tree.rb
@@ -1,7 +1,14 @@
 class Tree
+
+attr_accessor :type, :height, :age, :water_amount
+
   def initialize(type, height, age)
+    @type = type
+    @height = height
+    @age = age
   end
 
   def grow_tree(water_amount)
+      @height += water_amount/10
   end
 end
